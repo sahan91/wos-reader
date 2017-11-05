398cb3177be0252d0bbc33847adaf7e44fc22d3e
Sahan Serasinghe <sahan.serasinghe@gmail.com>
2017-11-06 00:46:44 +1030

    added renamer code

---
+++ wos-renamer.py
@@ -0,0 +1,14 @@
+import os
+
+arr = os.listdir('raw data')
+
+for folder in arr:
+    dir_path = os.path.join('raw data', folder)
+    files = os.listdir(dir_path)
+    count = 1
+    for file in files:
+        file_path_orig = os.path.join(dir_path, file)
+        file_path_dest = os.path.join(dir_path, folder+"_"+str(count)+".txt")
+        count += 1
+        os.rename(file_path_orig, file_path_dest)
+        
